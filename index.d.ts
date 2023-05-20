fd1ef0a2b18355cfa3d1b4081986c62e90170a38
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2023-05-21 00:30:51 +0200

    update types

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ interface CompassProps {
   size?: 'sm' | 'md' | 'lg'
   visualizePitch?: boolean
   wrapperClass?: string
+  onNeedleClick?: () => void
 }
 declare function Compass(props: CompassProps): React.JSX.Element
 export default Compass
