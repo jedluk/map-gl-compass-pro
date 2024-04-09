c698847cea00152159fd8ab886e7dd0bd434a168
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-04-09 18:40:13 +0200

    major project update

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  return {
+    publicDir: command === 'serve',
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/Compass.tsx'),
+        name: 'map-gl-compass-pro',
+        fileName: 'compass'
+      },
+      rollupOptions: {
+        external: ['react-map-gl', 'maplibre-gl', 'react', 'react-dom']
+      }
+    },
+    plugins: [
+      react(),
+      command === 'build' &&
+        dts({
+          rollupTypes: true,
+          outDir: __dirname
+        })
+    ]
+  }
+})
